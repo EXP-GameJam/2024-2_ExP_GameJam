693ad2f70a7a14acf7cca6636c15fea05eef2fd5
Kim Dasan <67263093+Plagem@users.noreply.github.com>
2024-08-16 16:47:57 +0900

    폴더 생성 & GameManager

---
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton 
+    private static GameManager _instance;
+    // Puclic Property
+    public static GameManager Instance
+    {
+        get
+        {
+            if(!_instance)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+                if (_instance == null)
+                    Debug.Log("No Singleton Object");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
