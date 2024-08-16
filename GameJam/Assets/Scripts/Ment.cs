cc9d8eb8b9ded40cb595c9129f8cb2ef4c6b318a
yuchan <rladbcks56@naver.com>
2024-08-16 22:00:23 +0900

    Ment

---
+++ GameJam/Assets/Scripts/Ment.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ment
+{
+    public static string[] ment = {
+        "절대 말 안 해!",
+        "난 잘못한 거 없어!",
+        "그냥 친구라니까!",
+        "이 손 놔 줘!!",
+        "숨 막혀 숨!!",
+        "헉 헉 미안해!!",
+        "살려주세요!",
+        "진짜 죽겠어",
+        "다 말할게!!!",
+        "네 친구 미영이랑 갔어",
+    };
+}
